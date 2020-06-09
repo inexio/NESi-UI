6f037a89cd30160c3555ba291b636d0ad6c83173
Juri Adams <juri@adams.sh>
2020-06-09 16:19:58 +0200

    :sparkles: Updated Subrack View

--- src/app/pages/subrack/subrack.component.ts
+++ src/app/pages/subrack/subrack.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ApiService } from "../../core/services/api/api.service";
+import { ActivatedRoute } from "@angular/router";
+import { RequestState } from "../../core/interfaces/request-state.type";
+import { Subrack } from "../../core/interfaces/subrack.interface";
 
 @Component({
-  selector: 'app-subrack',
-  templateUrl: './subrack.component.html',
-  styleUrls: ['./subrack.component.css']
+    selector: "app-subrack",
+    templateUrl: "./subrack.component.html",
+    styleUrls: ["./subrack.component.css"],
 })
 export class SubrackComponent implements OnInit {
+    constructor(public api: ApiService, private route: ActivatedRoute) {}
 
-  constructor() { }
+    public subrack: Subrack;
+    public subrackRequest: RequestState = "idle";
 
-  ngOnInit(): void {
-  }
+    public cards: { id: string }[] = [];
+    public cardsRequest: RequestState = "idle";
 
+    public parentDeviceId: string;
+
+    ngOnInit(): void {
+        this.route.params.subscribe((params) => {
+            this.parentDeviceId = params.id;
+
+            // Get Subrack
+            this.subrackRequest = "pending";
+            this.api.getSubrack(params.id, params.subrack).subscribe({
+                next: (subrack) => {
+                    this.subrack = subrack;
+                    this.subrackRequest = "success";
+                },
+                error: (error) => {
+                    console.error(error);
+                    this.subrackRequest = "error";
+                },
+            });
+
+            // Get Cards
+            this.cardsRequest = "pending";
+            this.api.getCards(params.id, params.subrack).subscribe({
+                next: (cards) => {
+                    this.cards = cards;
+                    this.cardsRequest = "success";
+                },
+                error: (error) => {
+                    console.error(error);
+                    this.cardsRequest = "error";
+                },
+            });
+        });
+    }
 }

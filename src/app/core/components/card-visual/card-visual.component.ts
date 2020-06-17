8f085b8d3dbbaca9e24dd0c66f9db8a5ab376ffa
Juri Adams <juri@adams.sh>
2020-06-17 17:12:08 +0200

    :sparkles: Updated Card visual Component

--- src/app/core/components/card-visual/card-visual.component.ts
+++ src/app/core/components/card-visual/card-visual.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, Input } from "@angular/core";
 import { Card } from "../../interfaces/card.interface";
 import { ApiService } from "../../services/api/api.service";
 import { RequestState } from "../../interfaces/request-state.type";
+import { ActivatedRoute } from "@angular/router";
 import { Port } from "../../interfaces/port.interface";
-import { zip } from "rxjs";
 
 @Component({
     selector: "app-card-visual",
@@ -11,45 +11,68 @@ import { zip } from "rxjs";
     styleUrls: ["./card-visual.component.css"],
 })
 export class CardVisualComponent implements OnInit {
-    @Input("parentDevice") public parentDevice: string;
-    @Input("cardId") public cardId: string;
+    /**
+     * Id of the parent device used to
+     */
+    public parentDeviceId: number | string;
 
-    @Input("card") public card: Card;
+    /**
+     * Card object, either given as input or retrieved through the API
+     */
+    @Input("card") public card?: Card;
+    @Input("cardId") public cardId?: number | string;
+
+    /**
+     * Request state of the Subrack if it has to be retrieved through the API
+     */
     public cardRequest: RequestState = "idle";
 
-    @Input("ports") public ports: Port[];
-    public portsRequest: RequestState = "idle";
+    public portPairs: [Port, Port][] = [];
 
-    constructor(private api: ApiService) {}
+    constructor(private route: ActivatedRoute, private api: ApiService) {}
 
     ngOnInit(): void {
-        if (!this.card && this.cardId) {
-            this.cardRequest = "pending";
-            this.api.getCard(this.parentDevice, this.cardId).subscribe({
-                next: (card) => {
-                    this.card = card;
-                    this.card.maxPorts = 32; // TODO: Remove
-                    this.cardRequest = "success";
-                },
-                error: (error) => {
-                    console.error(error);
-                    this.cardRequest = "error";
-                },
-            });
-        }
+        // Get id of parent device from route
+        this.route.params.subscribe((params) => {
+            this.parentDeviceId = params.id;
 
-        if (!this.ports && ((this.card && this.card.id) || this.cardId)) {
-            this.portsRequest = "pending";
-            this.api.getPorts(this.parentDevice, (this.card && this.card.id) || this.cardId).subscribe({
-                next: (ports) => {
-                    this.ports = ports;
-                    this.portsRequest = "success";
-                },
-                error: (error) => {
-                    console.error(error);
-                    this.portsRequest = error;
-                },
-            });
+            // Get Subrack if none is given
+            if (!this.card) {
+                // Throw error if either parentDeviceId or subrackId are missing
+                if (!this.parentDeviceId || !this.cardId) {
+                    throw new Error("Missing `parentDeviceId` or `cardId` which are needed to request Card data");
+                }
+
+                // Get Subrack data
+                this.cardRequest = "pending";
+                this.api.getCard(this.parentDeviceId, this.cardId).subscribe({
+                    next: (card) => {
+                        this.card = card;
+                        this.parsePorts();
+                        this.cardRequest = "success";
+                    },
+                    error: (error) => {
+                        console.error(error);
+                        this.cardRequest = "error";
+                    },
+                });
+            } else {
+                this.parsePorts();
+            }
+        });
+    }
+
+    /**
+     * Parse card ports into array of port pairs to display them
+     */
+    public parsePorts(): void {
+        for (let i = 0; i < Number(this.card.ppc); i++) {
+            if (!(i % 2)) {
+                this.portPairs.push([
+                    this.card.ports[i] ? this.card.ports[i] : { id: null, opr_state: null },
+                    this.card.ports[i - 1] ? this.card.ports[i - 1] : { id: null, opr_state: null },
+                ]);
+            }
         }
     }
 }

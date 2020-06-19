b308f7f453381b38cb1ceacfdeaa269c2dd9110a
Juri Adams <juri@adams.sh>
2020-06-19 16:39:13 +0200

    💡 Added Comments to CoreService

--- src/app/core/services/core/core.service.ts
+++ src/app/core/services/core/core.service.ts
@@ -4,7 +4,15 @@ import { Injectable, ElementRef } from "@angular/core";
     providedIn: "root",
 })
 export class CoreService {
+    /**
+     * Element Reference to the wrapper element of the Devices page(s)
+     */
     public devicesContentWrapper: ElementRef;
+
+    /**
+     * Boolean if any Http Request is currently pending
+     */
+    public requestPending: boolean = false;
 
     constructor() {}
 }

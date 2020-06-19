cc713c1175f680f7847b741cebc93ad2956e8986
Juri Adams <juri@adams.sh>
2020-06-19 16:38:01 +0200

    :sparkles: Updated menu styling for Electron and Browser

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from "@angular/core";
 import { ElectronService } from "./core/services";
 import { AppConfig } from "../environments/environment";
 import { AuthService } from "./core/services/auth/auth.service";
+import { CoreService } from "./core/services/core/core.service";
 
 @Component({
     selector: "app-root",
@@ -9,21 +10,18 @@ import { AuthService } from "./core/services/auth/auth.service";
     styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-    constructor(public electronService: ElectronService, private auth: AuthService) {
-        console.log("AppConfig", AppConfig);
-
-        if (electronService.isElectron) {
-            console.log(process.env);
-            console.log("Mode electron");
-            console.log("Electron ipcRenderer", electronService.ipcRenderer);
-            console.log("NodeJS childProcess", electronService.childProcess);
-        } else {
-            console.log("Mode web");
-        }
+    constructor(public electronService: ElectronService, private auth: AuthService, public core: CoreService) {
+        // if (electronService.isElectron) {
+        //     console.log(process.env);
+        //     console.log("Mode electron");
+        //     console.log("Electron ipcRenderer", electronService.ipcRenderer);
+        //     console.log("NodeJS childProcess", electronService.childProcess);
+        // } else {
+        //     console.log("Mode web");
+        // }
 
         this.auth.getStoredCredentials().subscribe({
             next: (credentials) => {
-                console.log("🔒 Found stored Credentials");
                 this.auth.credentials = credentials;
             },
             error: (error) => {

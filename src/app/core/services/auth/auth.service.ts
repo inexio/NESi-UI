ceed21d9ac6afcd7536f0edc5ca49e3d222e13f7
Juri Adams <juri@adams.sh>
2020-06-23 10:32:17 +0200

    ✨ Added new Credentials handling to AuthService

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 import { Credentials } from "../../interfaces/credentials.interface";
-import { HttpHeaders } from "@angular/common/http";
+import { HttpHeaders, HttpClient } from "@angular/common/http";
+import { delay, map } from "rxjs/operators";
 
 @Injectable({
     providedIn: "root",
@@ -12,8 +13,10 @@ export class AuthService {
      */
     public credentials: Credentials;
 
+    constructor(private http: HttpClient) {}
+
     /**
-     * Checks for Credentials object stored inside localStorage
+     * Check localStorage for `credentials` object
      */
     public getStoredCredentials(): Observable<Credentials> {
         return new Observable((subscriber) => {
@@ -44,5 +47,42 @@ export class AuthService {
                 subscriber.complete();
             }
         });
+    }
+
+    /**
+     * Make an example request to check given Credentials
+     * @param credentials Credentils object to perform test request with
+     */
+    public checkCredentials(credentials: Credentials): Observable<Credentials> {
+        return this.http
+            .get<any>(`${credentials.protocol}://${credentials.host}:${credentials.port}/softboxen/v1/boxen`, {
+                headers: new HttpHeaders().append(
+                    "Authorization",
+                    `Basic ${btoa(`${credentials.auth.username}:${credentials.auth.password}`)}`,
+                ),
+            })
+            .pipe(
+                delay(3000),
+                map(() => {
+                    return credentials;
+                }),
+            );
+    }
+
+    /**
+     * Store given Credentials object inside of localStorage
+     * @param credentials Credentials object to store in localStorage
+     */
+    public storeCredentials(credentials: Credentials): void {
+        localStorage.setItem("credentials", JSON.stringify(credentials));
+        this.getStoredCredentials().subscribe();
+    }
+
+    /**
+     * Remove stored credentials from localStorage and AuthService
+     */
+    public removeCredentials(): void {
+        localStorage.removeItem("credentials");
+        this.credentials = null;
     }
 }

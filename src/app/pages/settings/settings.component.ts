21aafade8e449e03604e36e2892a1a75df399684
Juri Adams <juri@adams.sh>
2020-06-23 10:27:22 +0200

    :sparkles: Added SettingsComponent

---
+++ src/app/pages/settings/settings.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from "@angular/core";
+import { buildInfo } from "../../../build";
+import { AuthService } from "../../core/services/auth/auth.service";
+import { NzModalService } from "ng-zorro-antd/modal";
+
+@Component({
+    selector: "app-settings",
+    templateUrl: "./settings.component.html",
+    styleUrls: ["./settings.component.css"],
+})
+export class SettingsComponent implements OnInit {
+    /**
+     * Import buildInfo
+     */
+    public build: any = buildInfo;
+
+    constructor(public auth: AuthService, private modal: NzModalService) {}
+
+    public confirmCredentialsRemoval(): void {
+        this.modal.confirm({
+            nzTitle: "Are you sure?",
+            nzContent: "You will have to enter your API information again.",
+            nzOkText: "Yes",
+            nzOkType: "danger",
+            nzOnOk: () => this.auth.removeCredentials(),
+            nzCancelText: "No",
+            nzAutofocus: null,
+        });
+    }
+
+    ngOnInit(): void {}
+}

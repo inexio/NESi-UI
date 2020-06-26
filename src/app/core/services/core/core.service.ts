8f6ea7d79f54d3bf9a0b52ed03b26b0020afcf36
Juri Adams <juri@adams.sh>
2020-06-26 09:35:55 +0200

    🔥 Removed ElectronService => CoreService

--- src/app/core/services/core/core.service.ts
+++ src/app/core/services/core/core.service.ts
@@ -1,4 +1,7 @@
 import { Injectable, ElementRef } from "@angular/core";
+import { ipcRenderer, webFrame, remote } from "electron";
+import * as childProcess from "child_process";
+import * as fs from "fs";
 
 @Injectable({
     providedIn: "root",
@@ -13,6 +16,40 @@ export class CoreService {
      * Boolean if any Http Request is currently pending
      */
     public requestPending: boolean = false;
+
+    /**
+     * Allow access to Electron `ipcRenderer` to communicate with main process
+     */
+    public ipcRenderer: typeof ipcRenderer;
+
+    /**
+     * Allow access to Electron `webFrame`
+     */
+    public webFrame: typeof webFrame;
+
+    /**
+     * Allow access to Electron `remote`
+     */
+    public remote: typeof remote;
+
+    /**
+     * Allow access to Node `childProcess`
+     */
+    public childProcess: typeof childProcess;
+
+    /**
+     * Allow access to Node filesystem
+     */
+    public fs: typeof fs;
+
+    /**
+     * Getter returning a boolean if whether the app is running on desktop or in the browser
+     *
+     * Special thanks to @maximegris/angular-electron
+     */
+    public get isElectron(): boolean {
+        return !!(window && window.process && window.process.type);
+    }
 
     constructor() {}
 }
